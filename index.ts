8e9277057a43da20056b397e7bb8504c59ebbee4
yanggengzhen <yanggz@chinagdn.com>
2022-01-21 18:21:37 +0800

    2022/1/21-1

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
 ;(() => {
-  // 2.2两数相加
+  // 9.24 两两交换链表中的节点
+  class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+      this.val = val === undefined ? 0 : val
+      this.next = next === undefined ? null : next
+    }
+  }
+
+  function swapPairs(head: ListNode | null): ListNode | null {}
 })()
