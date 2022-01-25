685498bd780a6351084d5019545bad82748f794f
yanggengzhen <yanggz@chinagdn.com>
2022-01-25 18:36:15 +0800

    20220125

--- index.ts
+++ index.ts
@@ -1,31 +1,15 @@
 ;(() => {
-  // 10.53 最大子数组和
-  const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-  // function maxSubArray(nums: number[]): number {
-  //   let ans = nums[0]
-  //   let sum = 0
-  //   for (const i of nums) {
-  //     if (sum > 0) {
-  //       sum += i
-  //     } else {
-  //       sum = i
-  //     }
-  //     ans = Math.max(ans, sum)
-  //   }
-  //   console.log(ans)
-  //   return ans
-  // }
-  function maxSubArray(nums: number[]): number {
-    const memo = []
-    memo[0] = nums[0]
-    for (let i = 1; i < nums.length; i++) {
-      memo[i] = Math.max(nums[i] + memo[i - 1], nums[i])
+  const intervals: number[][] = [
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ]
+  // 56. 合并区间
+  function merge(intervals: number[][]): number[][] {
+    let res: number[][] = []
+    for (let i in intervals){
+      intervals[i][1] intervals[i]
     }
-    let max = nums[0]
-    for (let i = 1; i < nums.length; i++) {
-      max = Math.max(max, memo[i])
-    }
-    return max
   }
-  maxSubArray(nums)
 })()
